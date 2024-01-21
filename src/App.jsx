ba7a66400011b80bd017cdd85f337eedf202b132
Abhishek Vats <virus@luffy-book.local>
2024-01-21 12:33:37 +0530

    Working with sharding and signature encryption is working

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { useEffect } from "react";
 import { encryptDataAndSendtoServer } from "encrypted-binary-communications";
 
 function App() {
-  const data = "hello World";
+  const data = { key1: "value1", key2: "value2", key3: "value3" };
   useEffect(() => {
-    encryptDataAndSendtoServer(data);
+    // encryptDataAndSendtoServer(ctx, req, endpoint, state, data)
+    encryptDataAndSendtoServer(0, 0, 0, 4, data);
   }, []);
 
   return <div>Hello</div>;
